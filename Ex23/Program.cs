3f5bf10af06fd64d37427d1316368c7b19ec887a
pogorelov <89607888555@mail.ru>
2023-04-15 17:32:04 +0700

    Ex23 done

---
+++ Ex23/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = GetUserNumber("Введите значение N: ");
+double pow;
+
+Console.Write($"{n} -> ");
+for (int i = 1; i < n; i++)
+{
+    pow = Math.Pow(i, 3);
+    Console.Write($"{pow} ");
+}
+
+static int GetUserNumber(string message)
+{
+    while (true)
+    {
+        Console.Write(message);
+        int userInput;
+        if (int.TryParse(Console.ReadLine(), out userInput))
+        {
+            return userInput;
+        }
+        else Console.WriteLine("ОШИБКА! ВЫ ВВЕЛИ НЕКОРРЕКТНЫЕ ДАННЫЕ");
+    }
+}
